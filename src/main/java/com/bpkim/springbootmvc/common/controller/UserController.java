3b79217fc2d01108743c031ae8838134e448516a
bpkim <kbp0213@gmail.com>
2021-08-30 00:39:33 +0900

    auth 사용자 정보 파라메터로 받기

--- src/main/java/com/bpkim/springbootmvc/common/controller/UserController.java
+++ src/main/java/com/bpkim/springbootmvc/common/controller/UserController.java
@@ -1,19 +1,44 @@
 package com.bpkim.springbootmvc.common.controller;
 
-import org.springframework.context.annotation.Profile;
+import com.bpkim.springbootmvc.common.accounts.Account;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@Profile("local")
+//@Profile("local")
 public class UserController {
 
+    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @ResponseBody
-    @RequestMapping("/user/info")
+    @GetMapping("/user/info")
     public Object userInfo(){
         return SecurityContextHolder.getContext().getAuthentication();
     }
+    @ResponseBody
+    @GetMapping("/user/auth")
+    public Object  userAuth(@AuthenticationPrincipal Account account){
+        logger.info(account.getUsername());
+        logger.info(account.getEmail());
+        logger.info(account.getId().toString());
+        return account;
+    }
+/*
+
+    @GetMapping(value = "/sample")
+    public void sample(@AuthenticationPrincipal MemberDTO memberDTO, Model model){
+
+        log.info("Sample....");
+        log.info(memberDTO);
+
+        model.addAttribute("member", memberDTO);
+    }
+    <div th:text="${member}"></div>
+*/
 
 }
